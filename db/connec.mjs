4ae744d82f8e360205d5472675232df1efde49c3
ludevique <makamludovique@gmail.com>
2024-07-23 12:45:20 -0700

    connected  server with database successfull

---
+++ db/connec.mjs
@@ -0,0 +1,18 @@
+//--------------------------Connecting my server with my database--------------------
+  //----First import mongoose to interact with my database--------------
+import mongoose from "mongoose";
+  //-----------Set dotenv to load variables i have inside the file--------------
+import dotenv from 'dotenv'
+  //------------Loading my hidden variables from the .env file into my app------------
+dotenv.config()
+
+
+//---------------Create a function which will helps me to display or check  whether my app is connect or not-------
+export function connectDB () {
+    mongoose.connect(process.env.ATLAS_URI);
+
+    const conn = mongoose.connection;
+    conn.on("error", (e) => console.error(e));
+    conn.on('open', () => console.log('successfully connected to the database'));
+    conn.on('close', () => console.log('disconnected to database'))
+} 
---
+++ server.mjs
@@ -0,0 +1,22 @@
+//--------------------------------------Setting up my server------------------------------------------------------------------------------
+import express from 'express'
+import bodyParser from 'body-parser';
+
+//-----------------------Create my App using the express object-----------------------------------------------------------------------------
+const App = express ();
+
+//--------------------------Specify the PORT of my application which i will set to '3000'---------------------------------------------------
+const PORT = 3000;
+
+//---------import my connectDB function to my server file-------------------------------------------------------------------------------
+import { connectDB } from './db/connec.mjs';
+connectDB()
+//--------------------Specify we are going to used JSON data in our application--------------------------------------------------------------
+App.use(bodyParser.json())
+
+//--------------Make our app listening to for incoming request-------------------------------------------------------------------------------
+App.listen(PORT, (err) => {
+    if(err) {resizeBy.status(500).send(`server is nopt working`)
+
+    }else console.log(`server is running`);
+})
